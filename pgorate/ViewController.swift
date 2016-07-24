b06a441e683db6c7b7241f5422a8dc2773ecc9e6
Rowell Heria <owieh@me.com>
2016-07-24 17:35:26 +0100

    Initial Commit

---
+++ pgorate/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pgorate
+//
+//  Created by Rowell Heria on 24/07/2016.
+//  Copyright © 2016 Rowell Heria. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
