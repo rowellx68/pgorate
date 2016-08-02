87290653ac53ae6b29292611a4ee12368cb1a6dd
Rowell Heria <owieh@me.com>
2016-08-02 16:15:13 +0100

    now displays the nickname

--- pgorate/PokemonDataTableViewController.swift
+++ pgorate/PokemonDataTableViewController.swift
@@ -29,8 +29,13 @@ class PokemonDataTableViewController: UITableViewController {
         let overall = Float(pokeData.individualAttack + pokeData.individualStamina + pokeData.individualDefense)/45.0
         
         cell.pokeImage.image = UIImage(named: "\(pokeData.pokemonId.hashValue)")
-        cell.name.text = pokeData.pokemonId.description.stringByReplacingOccurrencesOfString(".", withString: "")
         cell.percent.text = "\(String(format: "%.2f", overall * 100))%"
+        
+        if pokeData.hasNickname {
+            cell.name.text = pokeData.nickname
+        } else {
+            cell.name.text = pokeData.pokemonId.description.stringByReplacingOccurrencesOfString(".", withString: "")
+        }
         
         return cell
     }
