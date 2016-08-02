796c077dafef0162a165365476031fa67b68ad9f
Rowell Heria <owieh@me.com>
2016-08-02 12:28:36 +0100

    re-written GPSOAuth to use Alamofire

--- pgorate/pgoapi/gpsoauth.swift
+++ pgorate/pgoapi/gpsoauth.swift
@@ -1,13 +1,25 @@
-import Foundation
+//
+//  GPSOAuth.swift
+//  pgorate
+//
+//  Created by Rowell Heria on 02/08/2016.
+//  Copyright © 2016 Rowell Heria. All rights reserved.
+//
 
-class gpsoauth {
-    static let sharedInstance = gpsoauth()
-    var token = "",
-        accessToken = "",
-        email = ""
-    var expiry = 0
+import Foundation
+import Alamofire
+
+class GPSOAuth {
+    static let sharedInstance = GPSOAuth()
     
-    func parseKeyValues(body:String) -> Dictionary<String, String> {
+    var email: String!
+    var password: String!
+    var accessToken: String?
+    var token: String?
+    var expires: Int?
+    var loggedIn: Bool = false
+    
+    private func parseKeyValues(body:String) -> Dictionary<String, String> {
         var obj = [String:String]()
         let bodySplit = body.componentsSeparatedByString("\n")
         for values in bodySplit {
@@ -16,43 +28,78 @@ class gpsoauth {
         }
         return obj;
     }
-        
-    func getTicket(unescapedEmail: String, password: String) {
-        gpsoauth.sharedInstance.email = unescapedEmail.stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceCharacterSet()).stringByAddingPercentEncodingWithAllowedCharacters(.URLHostAllowedCharacterSet())!
-        self.request("accountType=HOSTED_OR_GOOGLE&Email=" + gpsoauth.sharedInstance.email + "&has_permission=1&add_account=1&Passwd=" + password.stringByAddingPercentEncodingWithAllowedCharacters(.URLHostAllowedCharacterSet())! + "&service=ac2dm&source=android&androidId=9774d56d682e549c&device_country=us&operatorCountry=us&lang=en&sdk_version=17")
-    }
     
-    func oauth () {
-        request("accountType=HOSTED_OR_GOOGLE&Email=" + gpsoauth.sharedInstance.email + "&EncryptedPasswd=" + gpsoauth.sharedInstance.token + "&has_permission=1&service=audience:server:client_id:848232511240-7so421jotr2609rmqakceuu1luuq0ptb.apps.googleusercontent.com&add_account=1&source=android&androidId=9774d56d682e549c&app=com.nianticlabs.pokemongo&client_sig=321187995bc7cdc2b5fc91b11a96e2baa8602c62&device_country=us&operatorCountry=us&lang=en&sdk_version=17")
-    }
-    
-    func request (requestData: String) {
-        let request = NSMutableURLRequest(URL: NSURL(string: "https://android.clients.google.com/auth")!)
-        let session = NSURLSession.sharedSession()
-        request.HTTPMethod = "POST"
+    func getTicket(email: String, password: String) {
+        GPSOAuth.sharedInstance.email = email.stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceCharacterSet()).stringByAddingPercentEncodingWithAllowedCharacters(.URLHostAllowedCharacterSet())!
+
+        let headers = [
+            "Content-Type": "application/x-www-form-urlencoded"
+        ]
         
-        request.HTTPBody = requestData.dataUsingEncoding(NSUTF8StringEncoding)
-        request.addValue("application/x-www-form-urlencoded", forHTTPHeaderField: "Content-Type")
+        let params = [
+            "accountType": "HOSTED_OR_GOOGLE",
+            "Email": GPSOAuth.sharedInstance.email,
+            "has_permission": "1",
+            "add_account": "1",
+            "Passwd": password.stringByAddingPercentEncodingWithAllowedCharacters(.URLHostAllowedCharacterSet())!,
+            "service": "ac2dm",
+            "source": "android",
+            "androidId": "9774d56d682e549c",
+            "device_country": "us",
+            "operatorCountry": "us",
+            "lang": "en",
+            "sdk_version": "17"
+        ]
         
-        let task = session.dataTaskWithRequest(request, completionHandler: {data, response, error -> Void in
-            let responseString = NSString(data: data!, encoding: NSUTF8StringEncoding)
+        Alamofire.request(.POST, Endpoint.GoogleLogin, parameters: params, headers: headers, encoding: .URLEncodedInURL)
+        .responseJSON { (response) in
+            let responseString = NSString(data: response.data!, encoding: NSUTF8StringEncoding)
+//            print(responseString)
             let googleDict = self.parseKeyValues(responseString! as String)
+            
             if googleDict["Token"] != nil {
-                gpsoauth.sharedInstance.token = googleDict["Token"]!
-                print("Received google token")
-                self.oauth()
-            } else if googleDict["Auth"] != nil {
-                gpsoauth.sharedInstance.accessToken = googleDict["Auth"]!
-                gpsoauth.sharedInstance.expiry = Int(googleDict["Expiry"]!)!
-                print("Received google auth, expiry timestamp: " + googleDict["Expiry"]!)
-                Auth.sharedInstance.loggedIn = true
-                Auth.sharedInstance.delegate?.didReceiveAuth()
+                self.loginOAuth(googleDict["Token"]!)
             } else {
-                print("Did not receive token/auth, login failed.")
                 Auth.sharedInstance.delegate?.didNotReceiveAuth()
             }
-        })
+        }
+    }
+    
+    func loginOAuth(token: String) {
+        let headers = [
+            "Content-Type": "application/x-www-form-urlencoded"
+        ]
         
-        task.resume()
+        let params = [
+            "accountType": "HOSTED_OR_GOOGLE",
+            "Email": GPSOAuth.sharedInstance.email,
+            "EncryptedPasswd": token,
+            "has_permission": "1",
+            "add_account": "1",
+            "source": "android",
+            "service": "audience:server:client_id:848232511240-7so421jotr2609rmqakceuu1luuq0ptb.apps.googleusercontent.com",
+            "androidId": "9774d56d682e549c",
+            "app": "com.nianticlabs.pokemongo",
+            "client_sig": "321187995bc7cdc2b5fc91b11a96e2baa8602c62",
+            "device_country": "us",
+            "operatorCountry": "us",
+            "lang": "en",
+            "sdk_version": "17"
+        ]
+        
+        Alamofire.request(.POST, Endpoint.GoogleLogin, parameters: params, headers: headers, encoding: .URLEncodedInURL)
+            .responseJSON { (response) in
+                let responseString = NSString(data: response.data!, encoding: NSUTF8StringEncoding)
+                let googleDict = self.parseKeyValues(responseString! as String)
+                
+                if googleDict["Auth"] != nil {
+                    GPSOAuth.sharedInstance.accessToken = googleDict["Auth"]!
+                    GPSOAuth.sharedInstance.expires = Int(googleDict["Expiry"]!)!
+                    Auth.sharedInstance.loggedIn = true
+                    Auth.sharedInstance.delegate?.didReceiveAuth()
+                } else {
+                    Auth.sharedInstance.delegate?.didNotReceiveAuth()
+                }
+        }
     }
 }
