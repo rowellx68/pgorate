857f43f29fad40409970ea9a2ca9c37fe63d1938
Rowell Heria <owieh@me.com>
2016-08-03 20:24:28 +0100

    initial work for the settings view

---
+++ pgorate/SettingsViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsViewController.swift
+//  pgorate
+//
+//  Created by Rowell Heria on 03/08/2016.
+//  Copyright © 2016 Rowell Heria. All rights reserved.
+//
+
+import Foundation
+import Eureka
+
+class SettingsViewController: FormViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
